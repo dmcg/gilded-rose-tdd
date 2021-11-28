c6e0d9df455183aef6904f5a2354ba145865cd0a
Duncan McGregor <duncan@oneeyedmen.com>
2021-11-28 22:25:48 +0000

    Better table formatting

--- src/main/java/com/gildedrose/Server.kt
+++ src/main/java/com/gildedrose/Server.kt
@@ -10,6 +10,8 @@ import org.http4k.routing.bind
 import org.http4k.routing.routes
 import org.http4k.server.Undertow
 import org.http4k.server.asServer
+import org.http4k.template.HandlebarsTemplates
+import org.http4k.template.ViewModel
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 import java.time.format.FormatStyle
@@ -24,10 +26,10 @@ class Server(
     val routes = routes(
         "/" bind Method.GET to { _ ->
             val now = clock()
-            Response(Status.OK).body(rootTemplate.apply(
-                mapOf(
-                    "now" to dateFormat.format(now),
-                    "items" to stock.map { it.toMap(now) }
+            Response(Status.OK).body(handlebars(
+                StockListViewModel(
+                    now = dateFormat.format(now),
+                    items = stock.map { it.toMap(now) }
                 )
             ))
         }
@@ -39,11 +41,13 @@ class Server(
         http4kServer.start()
     }
 
-    private val handlebars = Handlebars()
-    private val rootTemplate: Template = handlebars.compile(
-        StringTemplateSource("no such file", templateSource)
-    )
+    private val handlebars = HandlebarsTemplates().HotReload("src/main/java")
 }
+
+data class StockListViewModel(
+    val now: String,
+    val items: List<Map<String, String>>
+): ViewModel
 
 private fun Item.toMap(now: LocalDate): Map<String, String> = mapOf(
     "name" to name,
@@ -53,26 +57,6 @@ private fun Item.toMap(now: LocalDate): Map<String, String> = mapOf(
 )
 
 val templateSource = """
-    <html lang="en">
-    <body>
-    <h1>{{this.now}}</h1>
-    <table>
-    <tr>
-        <th>Name</th>
-        <th>Sell By Date</th>
-        <th>Sell By Days</th>
-        <th>Quality</th>
-    </tr>
-    {{#each this.items}}<tr>
-        <td>{{this.name}}</td>
-        <td>{{this.sellByDate}}</td>
-        <td>{{this.sellByDays}}</td>
-        <td>{{this.quality}}</td>
-    </tr>
-    {{/each}}
-    </table>
-    </body>
-    </html>
 
 """.trimIndent()
 
