4e3989632bbc26d7cfb6c477ac4c4e339cf71447
Duncan McGregor <duncan@oneeyedmen.com>
2022-02-02 14:29:11 +0000

    Upgrade stock file by saving when loaded

--- src/main/java/main.kt
+++ src/main/java/main.kt
@@ -1,11 +1,13 @@
 import com.gildedrose.Server
 import com.gildedrose.loadItems
 import com.gildedrose.routes
+import com.gildedrose.saveTo
 import java.io.File
 
 fun main() {
     val file = File("stock.tsv").also { it.createNewFile() }
     val stock = file.loadItems()
+    stock.saveTo(file)
 
     val server = Server(routes(stock))
     server.start()
