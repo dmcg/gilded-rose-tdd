53a9adcfccf744e46553f0488fb89f209d9fde9a
Duncan McGregor <duncan@oneeyedmen.com>
2022-02-06 11:01:55 +0000

    Bring more of main under test

--- src/main/java/main.kt
+++ src/main/java/main.kt
@@ -2,13 +2,20 @@ import com.gildedrose.Server
 import com.gildedrose.loadItems
 import com.gildedrose.routes
 import com.gildedrose.saveTo
+import org.http4k.routing.RoutingHttpHandler
 import java.io.File
+import java.time.LocalDate
 
 fun main() {
-    val file = File("stock.tsv").also { it.createNewFile() }
-    val stock = file.loadItems()
-    stock.saveTo(file)
-
-    val server = Server(routes(stock))
+    val file = File("stock.tsv")
+    val server = Server(routesFor(file))
     server.start()
+}
+
+fun routesFor(
+    stockFile: File,
+    calender: () -> LocalDate = LocalDate::now
+): RoutingHttpHandler {
+    val stock = stockFile.loadItems()
+    return routes(stock, calender)
 }
--- src/test/java/com/gildedrose/ListStockTests.kt
+++ src/test/java/com/gildedrose/ListStockTests.kt
@@ -8,21 +8,30 @@ import org.http4k.testing.Approver
 import org.http4k.testing.assertApproved
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
+import org.junit.jupiter.api.io.TempDir
+import routesFor
+import java.io.File
+import java.time.Instant
 import java.time.LocalDate
 
 @ExtendWith(ApprovalTest::class)
 class ListStockTests {
 
+    @TempDir
+    lateinit var dir: File
+    private val stockFile by lazy { dir.resolve("stock.tsv") }
     private val now = LocalDate.parse("2021-10-29")
 
     @Test
     fun `list stock`(approver: Approver) {
-        val stock = listOf(
-            Item("banana", now.minusDays(1), 42u),
-            Item("kumquat", now.plusDays(1), 101u)
-        )
-        val routes = routes(stock) { now }
-        val response = routes(Request(GET, "/"))
-        approver.assertApproved(response, OK)
+        StockList(
+            Instant.now(),
+            listOf(
+                Item("banana", now.minusDays(1), 42u),
+                Item("kumquat", now.plusDays(1), 101u)
+            )
+        ).saveTo(stockFile)
+        val routes = routesFor(stockFile) { now }
+        approver.assertApproved(routes(Request(GET, "/")), OK)
     }
 }
