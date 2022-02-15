0907458a81bb8d019483cc29291ac9ef125c6552
Duncan McGregor <duncan@oneeyedmen.com>
2022-02-15 17:50:27 +0000

    Stock now updates file on disk

---
+++ src/test/java/com/gildedrose/Stock.kt
@@ -0,0 +1,44 @@
+package com.gildedrose
+
+import java.io.File
+import java.time.Instant
+import java.time.LocalDate
+import java.time.ZoneId
+
+class Stock(
+    private val stockFile: File,
+    private val zoneId: ZoneId,
+    private val update: (items: List<Item>, days: Int) -> List<Item>
+) {
+    fun stockList(now: Instant): StockList {
+        val loaded = stockFile.loadItems()
+        val daysOutOfDate = loaded.lastModified.daysTo(now, zoneId)
+        val potentiallyUpdatedStockList = when {
+            daysOutOfDate > 0L -> loaded.copy(
+                lastModified = now,
+                update(loaded.items, daysOutOfDate.toInt())
+            )
+            else -> loaded
+        }
+        if (potentiallyUpdatedStockList.lastModified != loaded.lastModified)
+            save(potentiallyUpdatedStockList, now)
+        return potentiallyUpdatedStockList
+    }
+
+    private fun save(stockList: StockList, now: Instant) {
+        val versionFile = File.createTempFile(
+            "${stockFile.nameWithoutExtension}-$now-",
+            "." + stockFile.extension,
+            stockFile.parentFile
+        )
+        stockList.saveTo(versionFile)
+        val tempFile = File.createTempFile("tmp", "." + stockFile.extension)
+        stockList.saveTo(tempFile)
+        if (!tempFile.renameTo(stockFile))
+            error("Failed to rename temp to stockfile")
+    }
+}
+
+internal fun Instant.daysTo(that: Instant, zone: ZoneId): Long =
+    LocalDate.ofInstant(that, zone).toEpochDay() - LocalDate.ofInstant(this, zone).toEpochDay()
+
--- src/test/java/com/gildedrose/StockTests.kt
+++ src/test/java/com/gildedrose/StockTests.kt
@@ -2,47 +2,93 @@ package com.gildedrose
 
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
-import java.io.File
 import java.time.Instant
-import java.time.LocalDate
 import java.time.ZoneId
+import java.util.concurrent.Callable
+import java.util.concurrent.CyclicBarrier
+import java.util.concurrent.Executors
 
 
 class StockTests {
 
-    private val initialStockList = standardStockList.copy(
-        lastModified = Instant.parse("2022-02-09T23:59:59Z")
+    private val initialStockList = StockList(
+        lastModified = Instant.parse("2022-02-09T23:59:59Z"),
+        items = listOf(
+            Item("banana", oct29.minusDays(1), 42u),
+            Item("kumquat", oct29.plusDays(1), 101u)
+        )
     )
     private val fixture = Fixture(initialStockList)
-    private val stock = Stock(fixture.stockFile, ZoneId.of("Europe/London"))
 
-    @Test fun `loads stock from file`() {
+    private val stock = Stock(
+        stockFile = fixture.stockFile,
+        zoneId = ZoneId.of("Europe/London"),
+        update = ::updateItems
+    )
+
+    @Test
+    fun `loads stock from file`() {
         val now = Instant.parse("2022-02-09T23:59:59Z")
         assertEquals(initialStockList, stock.stockList(now))
     }
 
-    @Test fun `updates stock if last modified yesterday`() {
+    @Test
+    fun `updates stock if last modified yesterday`() {
         val now = Instant.parse("2022-02-10T00:00:01Z")
-        val expectedUpdatedResult = initialStockList.copy(lastModified = now)
+        val expectedUpdatedResult = StockList(
+            lastModified = now,
+            items = listOf(
+                Item("banana", oct29.minusDays(1), 41u),
+                Item("kumquat", oct29.plusDays(1), 100u)
+            )
+        )
         assertEquals(expectedUpdatedResult, stock.stockList(now))
         assertEquals(expectedUpdatedResult, fixture.load())
     }
-}
 
-class Stock(
-    private val stockFile: File,
-    private val zoneId: ZoneId
-) {
-    fun stockList(now: Instant): StockList {
-        val loaded = stockFile.loadItems()
-        return if (loaded.lastModified.daysTo(now, zoneId) == 0L)
-            loaded
-        else
-            loaded.copy(lastModified = now).also {
-                it.saveTo(stockFile)
+    @Test
+    fun `updates stock by two days if last modified the day before yesterday`() {
+        val now = Instant.parse("2022-02-11T00:00:01Z")
+        val expectedUpdatedResult = StockList(
+            lastModified = now,
+            items = listOf(
+                Item("banana", oct29.minusDays(1), 40u),
+                Item("kumquat", oct29.plusDays(1), 99u)
+            )
+        )
+        assertEquals(expectedUpdatedResult, stock.stockList(now))
+        assertEquals(expectedUpdatedResult, fixture.load())
+    }
+
+    @Test
+    fun `does not update stock if modified tomorrow`() {
+        val now = Instant.parse("2022-02-08T00:00:01Z")
+        assertEquals(initialStockList, stock.stockList(now))
+        assertEquals(initialStockList, fixture.load())
+    }
+
+    @Test
+    fun `parallel execution`() {
+        val count = 8
+        val executor = Executors.newFixedThreadPool(count)
+        val barrier = CyclicBarrier(count)
+        val futures = executor.invokeAll(
+            (1..count).map {
+                Callable() {
+                    barrier.await()
+                    `updates stock if last modified yesterday`()
+                }
             }
+        )
+        futures.forEach { it.get() }
+    }
+
+    @Test
+    fun `sanity check`() {
+        for (i in 1 .. 10) {
+            `parallel execution`()
+        }
     }
 }
 
-internal fun Instant.daysTo(that: Instant, zone: ZoneId): Long =
-    LocalDate.ofInstant(that, zone).toEpochDay() - LocalDate.ofInstant(this, zone).toEpochDay()
+private fun updateItems(items: List<Item>, days: Int) = items.map { it.copy(quality = it.quality - days.toUInt()) }
