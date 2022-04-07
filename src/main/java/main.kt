95e01a4a3cb55e51b41f2e4d2cc4ebcdc46f570a
Duncan McGregor <duncan@oneeyedmen.com>
2022-04-07 12:09:01 +0100

    Install CatchAll filter in case of errors

--- src/main/java/main.kt
+++ src/main/java/main.kt
@@ -1,8 +1,13 @@
 import com.gildedrose.*
 import org.http4k.core.HttpHandler
 import org.http4k.core.Method.GET
+import org.http4k.core.Response
+import org.http4k.core.Status.Companion.INTERNAL_SERVER_ERROR
+import org.http4k.core.then
+import org.http4k.filter.ServerFilters
 import org.http4k.routing.bind
 import org.http4k.routing.routes
+import org.slf4j.LoggerFactory
 import java.io.File
 import java.time.Instant
 import java.time.ZoneId
@@ -20,7 +25,16 @@ fun routesFor(
     clock: () -> Instant
 ): HttpHandler {
     val stock = Stock(stockFile, londonZoneId, ::updateItems)
-    return routes(
-        "/" bind GET to listHandler(clock, londonZoneId, stock::stockList)
+    return catchAllFilter.then(
+        routes(
+            "/" bind GET to listHandler(clock, londonZoneId, stock::stockList),
+            "/error" bind GET to { error("deliberate") }
+        )
     )
+}
+
+val logger = LoggerFactory.getLogger("Uncaught Exceptions")
+val catchAllFilter = ServerFilters.CatchAll {
+    logger.error("Uncaught exception", it)
+    Response(INTERNAL_SERVER_ERROR).body("Something went wrong, sorry.")
 }
