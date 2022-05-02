c6de3a40dac55984fe45f34981609a0ff3ad15dd
Duncan McGregor <duncan@oneeyedmen.com>
2022-05-02 10:14:57 +0100

    Extract AgedBrieUpdatingTests.kt

---
+++ src/test/java/com/gildedrose/AgedBrieUpdatingTests.kt
@@ -0,0 +1,32 @@
+package com.gildedrose
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class AgedBrieUpdatingTests {
+
+    @Test fun `increases in quality by one every day until its sell by date`() {
+        assertEquals(
+            Item("Aged Brie", oct29, 43),
+            Item("Aged Brie", oct29, 42).updatedBy(days = 1, on = oct29)
+        )
+    }
+
+    @Test fun `increases in quality by two every day after its sell by date`() {
+        assertEquals(
+            Item("Aged Brie", oct29, 44),
+            Item("Aged Brie", oct29, 42).updatedBy(days = 1, on = oct29.plusDays(1))
+        )
+    }
+
+    @Test fun `doesn't get better than 50`() {
+        assertEquals(
+            Item("Aged Brie", oct29, 50),
+            Item("Aged Brie", oct29, 50).updatedBy(days = 1, on = oct29)
+        )
+        assertEquals(
+            Item("Aged Brie", oct29, 50),
+            Item("Aged Brie", oct29, 49).updatedBy(days = 1, on = oct29.plusDays(1))
+        )
+    }
+}
--- src/test/java/com/gildedrose/StandardUpdatingTests.kt
+++ src/test/java/com/gildedrose/StandardUpdatingTests.kt
@@ -3,7 +3,7 @@ package com.gildedrose
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
-class UpdatingTests {
+class StandardUpdatingTests {
 
     @Test fun `items decrease in quality one per day`() {
         assertEquals(
@@ -54,31 +54,6 @@ class UpdatingTests {
         assertEquals(
             Item("banana", null, 42),
             Item("banana", null, 42).updatedBy(days = 1, on = oct29)
-        )
-    }
-
-    @Test fun `Aged Brie increase in quality by one every day until its sell by date`() {
-        assertEquals(
-            Item("Aged Brie", oct29, 43),
-            Item("Aged Brie", oct29, 42).updatedBy(days = 1, on = oct29)
-        )
-    }
-
-    @Test fun `Aged Brie increase in quality by two every day after its sell by date`() {
-        assertEquals(
-            Item("Aged Brie", oct29, 44),
-            Item("Aged Brie", oct29, 42).updatedBy(days = 1, on = oct29.plusDays(1))
-        )
-    }
-
-    @Test fun `Aged Brie doesn't get better than 50`() {
-        assertEquals(
-            Item("Aged Brie", oct29, 50),
-            Item("Aged Brie", oct29, 50).updatedBy(days = 1, on = oct29)
-        )
-        assertEquals(
-            Item("Aged Brie", oct29, 50),
-            Item("Aged Brie", oct29, 49).updatedBy(days = 1, on = oct29.plusDays(1))
         )
     }
 }
