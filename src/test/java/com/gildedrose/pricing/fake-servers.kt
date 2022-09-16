f20670d26bb568d7d5f76ab8398a970b128f04a8
Duncan McGregor <duncan@oneeyedmen.com>
2022-09-16 11:40:39 +0100

    Introduce random errors in fake server

--- src/test/java/com/gildedrose/pricing/fake-servers.kt
+++ src/test/java/com/gildedrose/pricing/fake-servers.kt
@@ -6,15 +6,16 @@ import com.gildedrose.domain.Price
 import com.gildedrose.domain.Quality
 import com.gildedrose.http.serverFor
 import org.http4k.routing.reverseProxy
+import kotlin.random.Random
 
 /**
  * Starts a fake server for demos
  */
 fun main() {
     val routes = reverseProxy(
-        "value-elf.com" to fakeValueElfRoutes(pricingWithMultiplier(100)),
-        "priceomatic.com" to fakeValueElfRoutes(pricingWithMultiplier(99)),
-        "webuyanymagicalitem.com" to fakeValueElfRoutes(pricingWithMultiplier(101)),
+        "value-elf.com" to fakeValueElfRoutes(pricingWithMultiplier(1000)),
+        "priceomatic.com" to fakeValueElfRoutes(pricingWithMultiplier(990)),
+        "webuyanymagicalitem.com" to fakeValueElfRoutes(pricingWithMultiplier(1010)),
     )
 
     serverFor(8080, routes).start()
@@ -22,7 +23,9 @@ fun main() {
 
 private fun pricingWithMultiplier(multiplier: Int): (ID<Item>, Quality) -> Price? = { id, quality ->
     when {
+        id.value.value == "banana" -> Price(709)!!
+        Random.nextDouble() > 0.99 -> error("Random failure")
         id.toString() == "no-such" -> null
-        else -> Price((id.value.length + 1) * multiplier.toLong() + quality.valueInt)!!
+        else -> Price((id.value.length + 1 + quality.valueInt) * multiplier.toLong())!!
     }
 }
