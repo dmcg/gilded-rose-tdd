497cdf2db0f182097f82e5ae2ee291f0c972fe74
Duncan McGregor <duncan@oneeyedmen.com>
2022-12-07 10:40:25 +0000

    Add performance testing to ParallelMapTests

--- src/test/java/com/gildedrose/foundation/ParallelMapTests.kt
+++ src/test/java/com/gildedrose/foundation/ParallelMapTests.kt
@@ -1,62 +1,117 @@
 package com.gildedrose.foundation
 
 import kotlinx.coroutines.*
+import org.junit.jupiter.api.*
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertTrue
-import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.condition.EnabledIfSystemProperty
-import java.util.concurrent.Callable
-import java.util.concurrent.ExecutorService
-import java.util.concurrent.Executors
+import java.util.concurrent.*
 import java.util.concurrent.atomic.AtomicReference
 import kotlin.concurrent.thread
 import kotlin.streams.toList
 import kotlin.system.measureTimeMillis
 
+
+@TestMethodOrder(MethodOrderer.MethodName::class)
 @EnabledIfSystemProperty(named = "run-slow-tests", matches = "true")
 class ParallelMapTests {
 
-    @Test
-    fun `kotlin version`() {
-        val timeMs = checkAndTime { map(it) }
-        assertTrue(timeMs >= 5000) { "Time was $timeMs" }
+    /* Note that this attempts to warm up HotSpot by running the non-parallel
+     * version first, and GCs between runs, but the results are subject to a
+     * lot of noise nonetheless.
+     */
+
+    private val listSize = 100
+    private val threadPool = ForkJoinPool(50)
+    private val sleepMs: Long = 25
+
+    @AfterEach
+    fun shutDown() {
+        threadPool.shutdown()
+        System.gc()
+        System.runFinalization()
     }
 
     @Test
-    fun `parallel stream version`() {
-        val timeMs = checkAndTime { parallelMapStream(it) }
-        assertTrue(timeMs < 2000) { "Time was $timeMs" }
+    fun `01 kotlin version`(testInfo: TestInfo) {
+        val parallelism = checkAndTime(testInfo) {
+            map(it)
+        }
+        assertTrue(parallelism <= 1.0)
     }
 
     @Test
-    fun `threads version`() {
-        val timeMs = checkAndTime { parallelMapThreads(it) }
-        assertTrue(timeMs < 2000) { "Time was $timeMs" }
+    fun `02 parallel stream version`(testInfo: TestInfo) {
+        val parallelism = checkAndTime(testInfo) {
+            parallelMapStream(it)
+        }
+        assertTrue(parallelism > 1)
     }
 
     @Test
-    fun `threadPool version`() {
-        val threadPool = Executors.newFixedThreadPool(50)
-        val timeMs = checkAndTime { parallelMapThreadPool(threadPool, it) }
-        assertTrue(timeMs < 2000) { "Time was $timeMs" }
+    fun `03 parallel stream forkJoinPool version`(testInfo: TestInfo) {
+        val parallelism = checkAndTime(testInfo) {
+            parallelMapStream(threadPool, it)
+        }
+        assertTrue(parallelism > 1)
     }
 
     @Test
-    fun `coroutines version`() {
-        val threadPool = Executors.newFixedThreadPool(50)
-        val timeMs = checkAndTime { f ->
-            runBlocking(threadPool.asCoroutineDispatcher()) {
-                parallelMapCoroutines(f)
+    fun `04 threads version`(testInfo: TestInfo) {
+        val parallelism = checkAndTime(testInfo) {
+            parallelMapThreads(it)
+        }
+        assertTrue(parallelism > 1)
+    }
+
+    @Test
+    fun `05 threadPool version`(testInfo: TestInfo) {
+        val parallelism = checkAndTime(testInfo) {
+            parallelMapThreadPool(threadPool, it)
+        }
+        assertTrue(parallelism > 1)
+    }
+
+    @Test
+    fun `06 coroutines version`(testInfo: TestInfo) {
+        // Coroutines run quicker when warmed up
+        runBlocking(threadPool.asCoroutineDispatcher()) {
+            coroutineScope {
+                for (i in 1..1000) {
+                    async { 1 }.await()
+                }
             }
         }
-        assertTrue(timeMs < 2000) { "Time was $timeMs" }
+        val parallelism = checkAndTime(testInfo) {
+            runBlocking(threadPool.asCoroutineDispatcher()) {
+                parallelMapCoroutines(it)
+            }
+        }
+        assertTrue(parallelism > 1)
     }
 
-    private fun checkAndTime(mapFunction: List<String>.((String) -> Int) -> List<Int>): Long {
-        val input = (1..100).map { it.toString() }
+    @Test
+    fun `07 coroutines delay version`(testInfo: TestInfo) {
+        val parallelism = checkAndTime(testInfo) {
+            runBlocking {
+                parallelMapCoroutines {
+                    delay(sleepMs)
+                    it.length
+                }
+            }
+        }
+        assertTrue(parallelism > 1)
+    }
+
+    private fun checkAndTime(
+        testInfo: TestInfo,
+        mapFunction: List<String>.((String) -> Int) -> List<Int>
+    ): Double {
+        val totalSleepMs = listSize * sleepMs
+        val input = (1..listSize).map { it.toString() }
         val output: List<Int>
         val timeMs = measureTimeMillis {
-            output = input.mapFunction { Thread.sleep(50); it.length }
+            output = input.mapFunction { Thread.sleep(sleepMs); it.length }
         }
         assertEquals(output.size, input.size)
         assertEquals(1, output[0])
@@ -64,12 +119,20 @@ class ParallelMapTests {
         assertEquals(2, output[9])
         assertEquals(2, output[98])
         assertEquals(3, output[99])
-        return timeMs
+        val parallelism = totalSleepMs / timeMs.toDouble()
+        return parallelism.also { println("${testInfo.displayName} : ${"%.1f".format(it)}") }
     }
 }
 
 fun <T, R> List<T>.parallelMapStream(f: (T) -> R) =
     stream().parallel().map(f).toList()
+
+
+fun <T, R> List<T>.parallelMapStream(
+    pool: ForkJoinPool,
+    f: (T) -> R
+): List<R> =
+    pool.submit(Callable { parallelMapStream(f) }).get()
 
 fun <T, R> Iterable<T>.parallelMapThreads(f: (T) -> R): List<R> =
     this.map {
