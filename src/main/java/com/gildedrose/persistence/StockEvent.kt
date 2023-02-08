7681a440fb6818b09d089908d5a4a7c7d415bc3e
Duncan McGregor <duncan@oneeyedmen.com>
2023-02-08 17:07:48 +0000

    Spike use of StockEvents to build Items

---
+++ src/main/java/com/gildedrose/persistence/StockEvent.kt
@@ -0,0 +1,40 @@
+package com.gildedrose.persistence
+
+import com.gildedrose.domain.ID
+import com.gildedrose.domain.Item
+import com.gildedrose.domain.NonBlankString
+import com.gildedrose.domain.Quality
+import java.time.Instant
+import java.time.LocalDate
+import java.time.ZonedDateTime
+
+sealed class StockEvent
+
+data class StockAdded(
+    val timestamp: Instant,
+    val id: ID<Item>,
+    val name: NonBlankString,
+    val sellByDate: LocalDate?,
+    val quality: Quality,
+) : StockEvent()
+
+data class StockRemoved(
+    val id: ID<Item>,
+) : StockEvent()
+
+fun Iterable<StockEvent>.toItems(now: ZonedDateTime): List<Item> {
+    val result = mutableListOf<Item>()
+    for (event in this) {
+        when (event) {
+            is StockAdded -> {
+                val itemWhenAdded = event.toItem()
+                val daysSinceAdded = event.timestamp.daysTo(now.toInstant(), now.zone)
+                result.add(itemWhenAdded.updatedBy(daysSinceAdded.toInt(), now.toLocalDate()))
+            }
+            is StockRemoved -> result.removeIf { it.id == event.id }
+        }
+    }
+    return result
+}
+
+private fun StockAdded.toItem() = Item(id, name, sellByDate, quality)
