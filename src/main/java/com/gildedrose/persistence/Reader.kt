d4130a1e4f4e8c01decfd65478c5d7e966caa4c1
Duncan McGregor <duncan@oneeyedmen.com>
2023-02-28 22:50:29 +0000

    Tidy

--- src/main/java/com/gildedrose/persistence/Reader.kt
+++ src/main/java/com/gildedrose/persistence/Reader.kt
@@ -4,16 +4,16 @@ import dev.forkhandles.result4k.Failure
 import dev.forkhandles.result4k.Result
 import dev.forkhandles.result4k.Success
 
-class Reader<TX, out R, out E>(
-    private val  f: context(TX) () -> Result<R, E>
+class Reader<C, out R, out E>(
+    private val  f: context(C) () -> Result<R, E>
 ) {
-    context(TX) fun run(): Result<R, E> {
-        return f(this@TX)
+    context(C) fun run(): Result<R, E> {
+        return f(this@C)
     }
 }
 
-fun <TX, R1, R2, E> Reader<TX, R1, E>.flatMap(f: (R1) -> Reader<TX, R2, E>) =
-    Reader<TX, R2, E> {
+fun <C, R1, R2, E> Reader<C, R1, E>.flatMap(f: (R1) -> Reader<C, R2, E>) =
+    Reader<C, R2, E> {
         val initialResult = this@flatMap.run()
         when (initialResult) {
             is Failure<E> -> initialResult
