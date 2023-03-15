3b184654801036d6a6964ca0956c01e19222593d
Duncan McGregor <duncan@oneeyedmen.com>
2023-03-15 09:05:07 +0000

    Add annotations for Data, Calculation, Action

---
+++ src/main/java/com/gildedrose/theory/annotations.kt
@@ -0,0 +1,20 @@
+package com.gildedrose.theory
+
+/**
+ * Immutable data.
+ */
+@Target(AnnotationTarget.CLASS)
+annotation class Data
+
+/**
+ * Code that will always return the same output for the same input.
+ * Does not depend on when or whether or how many times we run it.
+ */
+@Target(AnnotationTarget.FUNCTION, AnnotationTarget.CONSTRUCTOR)
+annotation class Calculation
+
+/**
+ * All code that is not a [Calculation].
+ */
+@Target(AnnotationTarget.FUNCTION, AnnotationTarget.CONSTRUCTOR)
+annotation class Action
