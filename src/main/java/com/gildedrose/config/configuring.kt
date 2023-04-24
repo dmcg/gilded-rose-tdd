b13d5b8db367bf28fd775d67ae84ae7e13a3d3a8
Duncan McGregor <duncan@oneeyedmen.com>
2023-04-24 17:17:43 +0100

    Configure HikariDataSource with http4k Environments

---
+++ src/main/java/com/gildedrose/config/configuring.kt
@@ -0,0 +1,25 @@
+package com.gildedrose.config
+
+import com.zaxxer.hikari.HikariDataSource
+import org.http4k.cloudnative.env.Environment
+import org.http4k.cloudnative.env.EnvironmentKey
+import org.http4k.lens.nonEmptyString
+import java.net.URI
+
+data class DbConfig(
+    val jdbcUrl: URI,
+    val username: String,
+    val password: String
+)
+
+fun Environment.toDbConfig() = DbConfig(
+    jdbcUrl = EnvironmentKey.map(URI::create).required("jdbc.url")(this),
+    username = EnvironmentKey.nonEmptyString().required("db.username")(this),
+    password = EnvironmentKey.nonEmptyString().required("db.password")(this),
+)
+
+fun DbConfig.toHikariDataSource() = HikariDataSource().apply {
+    jdbcUrl = this@toHikariDataSource.jdbcUrl.toString()
+    username = this@toHikariDataSource.username
+    password = this@toHikariDataSource.password
+}
--- src/test/java/com/gildedrose/persistence/jooq/JooqItemsTests.kt
+++ src/test/java/com/gildedrose/persistence/jooq/JooqItemsTests.kt
@@ -1,22 +1,31 @@
 package com.gildedrose.persistence.jooq
 
+import com.gildedrose.config.toDbConfig
+import com.gildedrose.config.toHikariDataSource
 import com.gildedrose.db.tables.Items
 import com.gildedrose.foundation.IO
 import com.gildedrose.persistence.ItemsContract
+import org.http4k.cloudnative.env.Environment
 import org.jooq.DSLContext
 import org.jooq.SQLDialect
 import org.jooq.impl.DSL
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
-import org.postgresql.ds.PGSimpleDataSource
 
-val testDataSource = PGSimpleDataSource().apply {
-    user = "gilded"
-    password = "rose"
-    databaseName = "gilded-rose"
-    portNumbers = intArrayOf(5433)
-}
-val testDslContext: DSLContext = DSL.using(testDataSource, SQLDialect.POSTGRES)
+val testEnvironment: Environment = Environment.JVM_PROPERTIES overrides
+    Environment.ENV overrides
+    Environment.from(
+        "jdbc.url" to "jdbc:postgresql://localhost:5433/gilded-rose",
+        "db.username" to "gilded",
+        "db.password" to "rose"
+    )
+
+val testDslContext: DSLContext = DSL.using(
+    testEnvironment.toDbConfig().toHikariDataSource().apply {
+        validate()
+    },
+    SQLDialect.POSTGRES
+)
 
 class JooqItemsTests : ItemsContract<JooqTXContext>(
     items = JooqItems(testDslContext)
