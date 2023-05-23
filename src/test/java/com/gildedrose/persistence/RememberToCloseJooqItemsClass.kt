b9d9ddfda5a23fa9417e3931c514921e367e3b74
Duncan McGregor <duncan@oneeyedmen.com>
2023-05-23 10:15:01 +0100

    Add RememberToCloseJooqItemsClass

---
+++ src/test/java/com/gildedrose/persistence/RememberToCloseJooqItemsClass.kt
@@ -0,0 +1,23 @@
+package com.gildedrose.persistence
+
+import com.gildedrose.persistence.jooq.JooqItems
+import org.junit.jupiter.api.Test
+import kotlin.test.assertFalse
+
+class RememberToCloseJooqItemsClass {
+
+    @Test
+    fun `JooqItems is not open when DualItemsTests doesn't exist`() {
+        val dualItemsTestsExists = try {
+            Class.forName("com.gildedrose.persistence.DualItemsTests")
+            true
+        } catch (x: ClassNotFoundException) {
+            false
+        }
+        if (!dualItemsTestsExists)
+            assertFalse(
+                JooqItems::class.isOpen,
+                "Remember to remove the open modifier on JooqItems now it isn't needed by the tests"
+            )
+    }
+}
