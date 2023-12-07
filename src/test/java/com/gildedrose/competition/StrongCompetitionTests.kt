773a5b830d6cb394f2ed0b59ade9de4b8422df13
Duncan McGregor <duncan@oneeyedmen.com>
2023-12-07 12:16:09 +0000

    LensObject andThen composition

--- src/test/java/com/gildedrose/competition/StrongCompetitionTests.kt
+++ src/test/java/com/gildedrose/competition/StrongCompetitionTests.kt
@@ -19,46 +19,88 @@ class StrongCompetitionTests {
     private val objectMapper = jacksonObjectMapper().apply {
         configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
     }
-
-    val dataFile = File(
+    private val dataFile = File(
         "src/test/resources/${this.javaClass.packageName.replace('.', '/')}",
         "places-response.json"
     )
-
-    @Test
-    fun `process data`(approver: Approver) {
-        val places: List<Place> = objectMapper.readValue<Root>(dataFile).places
-        approver.assertApproved(places.joinToString("\n") { "${it.displayName.text} ${it.countryCode}" })
-
-        val place = places.first()
-
-        val place2 =
-            place.copy(paymentOptions = place.paymentOptions.copy(acceptsNfc = !place.paymentOptions.acceptsNfc))
-        val place3 = acceptsNfc(place) { !it }
-        expectThat(place2).isEqualTo(place3)
-    }
+    private val places: List<Place> = objectMapper.readValue<Root>(dataFile).places
 
     private val Place.countryCode: String?
         get() = addressComponents.find { it.types.contains("country") }?.shortText
-}
 
-interface Lens<T, V> : (T) -> V {
-    operator fun invoke(subject: T, value: V): T
-    fun inject(subject: T, value: V): T = this(subject, value)
+    @Test
+    fun `process data`(approver: Approver) {
+        approver.assertApproved(
+            places.joinToString("\n") { "${it.displayName.text} ${it.countryCode}" }
+        )
+    }
 
-    operator fun invoke(subject: T, f: (V) -> V): T {
-        val current = this(subject)
-        val updated = f(current)
-        return this.inject(subject, updated)
+    @Test
+    fun lenses() {
+
+        val displayName = LensObject<Place, DisplayName>(
+            { it.displayName },
+            { subject, value -> subject.copy(displayName = value) }
+        )
+
+        val text = LensObject<DisplayName, String>(
+            { it.text },
+            { subject, value -> subject.copy(text = value) }
+        )
+
+        val displayNameTextToo = displayName andThen text
+
+        val aPlace: Place = places.first()
+
+        expectThat(aPlace.displayName.text).isEqualTo("International Magic Shop")
+        expectThat(displayNameTextToo.get(aPlace)).isEqualTo("International Magic Shop")
+
+        val editedPlace = aPlace.copy(
+            displayName = aPlace.displayName.copy(
+                text = "New name"
+            )
+        )
+        expectThat(editedPlace.displayName.text).isEqualTo("New name")
+
+        val editedWithLens = displayNameTextToo.inject(aPlace, "New name")
+        expectThat(displayNameTextToo.get(editedWithLens)).isEqualTo("New name")
+
+        val transformedPlace = aPlace.copy(
+            displayName = aPlace.displayName.copy(
+                text = aPlace.displayName.text.uppercase()
+            )
+        )
+        expectThat(transformedPlace.displayName.text).isEqualTo("INTERNATIONAL MAGIC SHOP")
+
+        val transformedWithLens = displayNameTextToo.update(aPlace) { it.uppercase() }
+        expectThat(displayNameTextToo.get(transformedWithLens)).isEqualTo("INTERNATIONAL MAGIC SHOP")
     }
 }
 
-object acceptsNfc : Lens<Place, Boolean> {
-    override operator fun invoke(subject: Place): Boolean = subject.paymentOptions.acceptsNfc
-    override operator fun invoke(subject: Place, value: Boolean): Place =
-        subject.withPaymentOptions { copy(acceptsNfc = value) }
+interface Lens<T, R> {
+    fun get(subject: T): R
+    fun inject(subject: T, value: R): T
+    fun update(subject: T, f: (R) -> R): T = inject(subject, f(get(subject)))
 }
 
-private fun Place.withPaymentOptions(f: (PaymentOptions).() -> PaymentOptions): Place =
-    this.copy(paymentOptions = f(this.paymentOptions))
+infix fun <T1, T2, R> LensObject<T1, T2>.andThen(second: LensObject<T2, R>) = LensObject<T1, R>(
+    { second.get(get(it)) },
+    { subject, value ->
+        inject(
+            subject,
+            second.inject(get(subject), value)
+        )
+    }
+)
+
+data class LensObject<T, R>(
+    val getter: (T) -> R,
+    val injector: (T, R) -> T
+) : Lens<T, R> {
+    override fun get(subject: T) = getter(subject)
+    override fun inject(subject: T, value: R) = injector(subject, value)
+}
+
+
+
 
