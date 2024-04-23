aa9b671b7a66b5ec69fe9aaa9a3ffbc7958ff7e2
Duncan McGregor <duncan@oneeyedmen.com>
2024-04-23 08:39:55 +0100

    H2 almost working

--- db/migrate-dev.sh
+++ db/migrate-dev.sh
@@ -3,4 +3,4 @@ set -e
 BASEDIR=$(dirname "$0")/..
 
 cd $BASEDIR
-JDBC_URL='jdbc:postgresql://localhost:5432/gilded-rose' ./gradlew flywayMigrate
+JDBC_URL="jdbc:h2:~/gilded-rose.db" ./gradlew flywayMigrate
