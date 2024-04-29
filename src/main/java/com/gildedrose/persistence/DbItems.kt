f54be933807271812c72bf213ac573ef91f131de
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2024-04-29 23:06:13 +0100

    refactoring with

--- src/main/java/com/gildedrose/persistence/DbItems.kt
+++ src/main/java/com/gildedrose/persistence/DbItems.kt
@@ -58,16 +58,14 @@ fun DSLContext.save(stockList: StockList) {
         else -> stockList.items
     }
     toSave.forEach { item ->
-        with(item) {
-            with(ITEMS) {
-                insertInto(this)
-                    .set(ID, id.toString())
-                    .set(MODIFIED, stockList.lastModified)
-                    .set(NAME, name)
-                    .set(QUALITY, quality.valueInt)
-                    .set(SELLBYDATE, sellByDate)
-                    .execute()
-            }
+        with(ITEMS) {
+            insertInto(this)
+                .set(ID, item.id.toString())
+                .set(MODIFIED, stockList.lastModified)
+                .set(NAME, item.name)
+                .set(QUALITY, item.quality.valueInt)
+                .set(SELLBYDATE, item.sellByDate)
+                .execute()
         }
     }
 }
