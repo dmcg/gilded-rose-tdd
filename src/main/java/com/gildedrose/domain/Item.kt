6bc11a20bf0e5a322da4190d55f7e219f52ef119
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2024-04-30 20:49:12 +0100

    refactoring

--- src/main/java/com/gildedrose/domain/Item.kt
+++ src/main/java/com/gildedrose/domain/Item.kt
@@ -5,7 +5,7 @@ import java.time.LocalDate
 
 data class Item(
     val id: ID<Item>,
-    val name: String,
+    val name: NonBlankString,
     val sellByDate: LocalDate?,
     val quality: Quality,
 )
@@ -18,6 +18,6 @@ data class PricedItem(
     val price: Result4k<Price?, Exception>,
 ) {
     constructor(item: Item, price: Result4k<Price?, Exception>) :
-        this(item.id, item.name, item.sellByDate, item.quality, price)
+        this(item.id, item.name.value, item.sellByDate, item.quality, price)
 }
 
