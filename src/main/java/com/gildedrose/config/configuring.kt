f7458ea28157a0f6183ec425435fa1d37ea5a650
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2024-04-30 11:24:52 +0100

    refactoring coupling

--- src/main/java/com/gildedrose/config/configuring.kt
+++ src/main/java/com/gildedrose/config/configuring.kt
@@ -13,13 +13,13 @@ data class DbConfig(
     val jdbcUrl: URI,
     val username: String,
     val password: String
-) {
-    constructor(environment: Environment) : this(
-        jdbcUrl = EnvironmentKey.map(URI::create).required("jdbc.url")(environment).printed(),
-        username = EnvironmentKey.nonEmptyString().required("db.username")(environment),
-        password = EnvironmentKey.nonEmptyString().required("db.password")(environment),
-    )
-}
+)
+
+fun DbConfig(environment: Environment) = DbConfig(
+    jdbcUrl = EnvironmentKey.map(URI::create).required("jdbc.url")(environment).printed(),
+    username = EnvironmentKey.nonEmptyString().required("db.username")(environment),
+    password = EnvironmentKey.nonEmptyString().required("db.password")(environment),
+)
 
 fun DbConfig.toDslContext() =
     HikariDataSource().also {
