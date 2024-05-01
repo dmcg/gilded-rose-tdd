69ef39b3ec21a6f36f92a538be5dd01255a701dc
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2024-05-01 22:42:43 +0100

    refactoring

--- src/main/java/com/gildedrose/domain/Quality.kt
+++ src/main/java/com/gildedrose/domain/Quality.kt
@@ -1,18 +1,14 @@
 package com.gildedrose.domain
 
 @JvmInline
-value class Quality(
-    val value: NonNegativeInt
-) {
+value class Quality(val value: NonNegativeInt) {
     val valueInt get() = value.value
 
     companion object {
         val ZERO: Quality = Quality(0)!!
 
-        operator fun invoke(value: Int): Quality? {
-            val wrapped = NonNegativeInt(value)
-            return if (wrapped != null) Quality(wrapped) else null
-        }
+        operator fun invoke(value: Int): Quality? =
+            NonNegativeInt(value)?.let(::Quality)
     }
 
     override fun toString() = value.toString()
