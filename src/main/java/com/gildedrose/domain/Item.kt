2af8d943171bb6262dfc849ab572e024b1337281
Duncan McGregor <duncan@oneeyedmen.com>
2024-05-05 11:05:27 +0100

    delme

--- src/main/java/com/gildedrose/domain/Item.kt
+++ src/main/java/com/gildedrose/domain/Item.kt
@@ -6,7 +6,12 @@ import java.time.LocalDate
 
 typealias ItemID = String
 
-data class ItemName(val value: String) : CharSequence by value
+data class ItemName(val value: String) : CharSequence by value {
+    init {
+        require(value.isNotBlank()) { "Name must not be blank" }
+    }
+
+}
 
 data class Item(
     val id: ItemID,
@@ -21,7 +26,6 @@ data class Item(
     ) : this(id, ItemName(name), sellByDate, quality)
     init {
         require(id.isNotBlank()) { "id must not be blank" }
-        require(_name.isNotBlank()) { "Name must not be blank" }
     }
     val name get() = _name.value
     fun copy(name: String): Item = this.copy(_name = ItemName(name))
@@ -29,17 +33,18 @@ data class Item(
 
 data class PricedItem(
     val id: ItemID,
-    val name: String,
+    val _name: ItemName,
     val sellByDate: LocalDate?,
     val quality: Quality,
     val price: Result4k<Price?, Exception>,
 ) {
     constructor(item: Item, price: Result4k<Price?, Exception>) :
-        this(item.id, item.name, item.sellByDate, item.quality, price)
+        this(item.id, item._name, item.sellByDate, item.quality, price)
+
+    val name get() = _name.value
 
     init {
         require(id.isNotBlank()) { "id must not be blank" }
-        require(name.isNotBlank()) { "Name must not be blank" }
     }
 }
 
