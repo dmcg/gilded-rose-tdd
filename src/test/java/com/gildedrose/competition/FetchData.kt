51175b6d842e56fd0c2bdd00097340a40d6ff7a6
Duncan McGregor <duncan@oneeyedmen.com>
2025-01-18 13:22:17 +0000

    Stop tests failing on class loading if google-api-key.txt isn't there

--- src/test/java/com/gildedrose/competition/FetchData.kt
+++ src/test/java/com/gildedrose/competition/FetchData.kt
@@ -12,10 +12,11 @@ class FetchData {
 
     @Test
     fun `fetch data`() {
+        val googleApiKey: String = File("./google-api-key.txt").readText()
         val client = ApacheClient()
         val query = "magical goods store europe"
         val request = Request(Method.POST, "https://places.googleapis.com/v1/places:searchText")
-            .header("X-Goog-Api-Key", GoogleApiKey)
+            .header("X-Goog-Api-Key", googleApiKey)
             .header("X-Goog-FieldMask", "*")
             .body("""{ "textQuery" : "$query"}""")
         val response = client(request)
@@ -23,7 +24,6 @@ class FetchData {
     }
 
     companion object {
-        private val GoogleApiKey: String = File("./google-api-key.txt").readText()
         val dataFile = File(
             "src/test/resources/${this::class.java.packageName.replace('.', '/')}",
             "places-response.json"
