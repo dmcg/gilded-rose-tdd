7ec3e3d049281e8491bb04192322e67164696110
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2025-05-23 16:20:24 +0200

    Junie, write a short commit message

--- src/main/java/main.kt
+++ src/main/java/main.kt
@@ -3,18 +3,18 @@ import com.gildedrose.config.DbConfig
 import com.gildedrose.http.serverFor
 import com.gildedrose.routes
 import org.http4k.config.Environment
+import org.http4k.config.Environment.Companion.ENV
+import org.http4k.config.Environment.Companion.JVM_PROPERTIES
 
 fun main() {
     val app = App(dbConfig = dbConfig)
     serverFor(port = 80, app.routes).start()
 }
 
-val dbConfig = DbConfig(
-    Environment.JVM_PROPERTIES overrides
-        Environment.ENV overrides
-        Environment.from(
-            "jdbc.url" to "jdbc:h2:/tmp/gilded-rose.db",
-            "db.username" to "gilded",
-            "db.password" to "rose"
-        )
+private val localEnv = Environment.from(
+    "jdbc.url" to "jdbc:h2:/tmp/gilded-rose.db",
+    "db.username" to "gilded",
+    "db.password" to "rose"
 )
+
+val dbConfig = DbConfig(JVM_PROPERTIES overrides ENV overrides localEnv)
