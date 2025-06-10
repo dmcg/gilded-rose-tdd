6688827b45021f94f4b1a5d734046da0285437a9
Duncan McGregor <duncan@oneeyedmen.com>
2025-06-10 12:05:34 +0100

    Refactor LoadingAndUpdatingStockTests to a more functional form

--- core/src/test/kotlin/com/gildedrose/updating/stock-tests.kt
+++ core/src/test/kotlin/com/gildedrose/updating/stock-tests.kt
@@ -1,13 +1,19 @@
 package com.gildedrose.updating
 
+import com.gildedrose.domain.Item
 import com.gildedrose.domain.StockList
+import com.gildedrose.domain.StockListLoadingError
 import com.gildedrose.testing.InMemoryItems
 import com.gildedrose.testing.item
 import com.gildedrose.testing.oct29
 import com.gildedrose.updating.StockUpdateDecision.DoNothing
 import com.gildedrose.updating.StockUpdateDecision.SaveUpdate
+import dev.forkhandles.result4k.Result4k
 import dev.forkhandles.result4k.asSuccess
+import dev.forkhandles.result4k.onFailure
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertAll
+import java.time.Instant
 import java.time.ZoneId
 import java.util.concurrent.Callable
 import java.util.concurrent.CyclicBarrier
@@ -19,13 +25,7 @@ class MaybeUpdateStockTests {
 
     @Test
     fun `loads stock but doesn't update if loaded same day as last modified`() {
-        val initialStockList = StockList(
-            lastModified = t("2021-02-09T00:00:00Z"),
-            items = listOf(
-                item("banana", oct29.minusDays(1), 42),
-                item("kumquat", oct29.plusDays(1), 101)
-            )
-        )
+        val initialStockList = StockList(t("2021-02-09T00:00:00Z"), someItems)
         assertEquals(
             DoNothing(initialStockList),
             mayBeUpdate(initialStockList, t("2021-02-09T00:00:00Z"), londonZone)
@@ -38,14 +38,8 @@ class MaybeUpdateStockTests {
 
     @Test
     fun `updates stock if last modified yesterday`() {
-        val initialStockList = StockList(
-            lastModified = t("2021-02-09T23:59:59Z"),
-            items = listOf(
-                item("banana", oct29.minusDays(1), 42),
-                item("kumquat", oct29.plusDays(1), 101)
-            )
-        )
-        val expectedItems = initialStockList.items.map { it.copy(quality = it.quality - 1) }
+        val initialStockList = StockList(t("2021-02-09T23:59:59Z"), someItems)
+        val expectedItems = initialStockList.items.withQualityDecreasedBy(1)
         assertEquals(
             SaveUpdate(
                 StockList(t("2021-02-10T00:00:00Z"), expectedItems)
@@ -62,14 +56,8 @@ class MaybeUpdateStockTests {
 
     @Test
     fun `updates stock by two days if last modified the day before yesterday`() {
-        val initialStockList = StockList(
-            lastModified = t("2021-02-09T23:59:59Z"),
-            items = listOf(
-                item("banana", oct29.minusDays(1), 42),
-                item("kumquat", oct29.plusDays(1), 101)
-            )
-        )
-        val expectedItems = initialStockList.items.map { it.copy(quality = it.quality - 2) }
+        val initialStockList = StockList(t("2021-02-09T23:59:59Z"), someItems)
+        val expectedItems = someItems.withQualityDecreasedBy(2)
         assertEquals(
             SaveUpdate(
                 StockList(t("2021-02-11T00:00:00Z"), expectedItems)
@@ -86,13 +74,7 @@ class MaybeUpdateStockTests {
 
     @Test
     fun `does not update stock if modified tomorrow`() {
-        val initialStockList = StockList(
-            lastModified = t("2021-02-09T00:00:00Z"),
-            items = listOf(
-                item("banana", oct29.minusDays(1), 42),
-                item("kumquat", oct29.plusDays(1), 101)
-            )
-        )
+        val initialStockList = StockList(t("2021-02-09T00:00:00Z"), someItems)
         assertEquals(
             DoNothing(initialStockList),
             mayBeUpdate(initialStockList, t("2021-02-08T00:00:00Z"), londonZone)
@@ -105,36 +87,35 @@ class MaybeUpdateStockTests {
 }
 
 class LoadingAndUpdatingStockTests {
-
-    private val initialStockList = StockList(
-        lastModified = t("2021-02-09T23:59:59Z"),
-        items = listOf(
-            item("banana", oct29.minusDays(1), 42),
-            item("kumquat", oct29.plusDays(1), 101)
-        )
-    )
-    private val items = InMemoryItems(initialStockList)
-    private val stock = Stock(items, zoneId = londonZone)
-
     @Test
     fun `updates stock if last modified yesterday`() {
-        val now = t("2021-02-10T00:00:01Z")
-        val expectedUpdatedResult = StockList(
-            lastModified = now,
-            items = initialStockList.items.map { it.copy(quality = it.quality - 1) }
-        ).asSuccess()
-        items.inTransaction {
-            assertEquals(
-                expectedUpdatedResult,
-                stock.loadAndUpdateStockList(now)
-            )
-        }
-        items.inTransaction {
-            assertEquals(
-                expectedUpdatedResult,
-                items.load()
-            )
-        }
+        // Given
+        val initialStockList = StockList(t("2021-02-09T23:59:59Z"), someItems)
+        val firstInstantOfNextDay = t("2021-02-10T00:00:00Z")
+
+        // When
+        val (result, nextState) = doLoadAndUpdate(initialStockList, firstInstantOfNextDay)
+
+        // Then
+        val expectedUpdatedResult = StockList(firstInstantOfNextDay, someItems.withQualityDecreasedBy(1))
+        assertAll(
+            { assertEquals(expectedUpdatedResult.asSuccess(), result) },
+            { assertEquals(expectedUpdatedResult, nextState) }
+        )
+    }
+
+    @Test
+    fun `does not update stock if last modified today`() {
+        val initialStockList = StockList(t("2021-02-09T00:00:00Z"), someItems)
+        val lastInstantOfSameDay = t("2021-02-09T23:59:59.9999Z")
+
+        val (result, nextState) = doLoadAndUpdate(initialStockList, lastInstantOfSameDay)
+
+        val expectedNotUpdatedResult = initialStockList
+        assertAll(
+            { assertEquals(expectedNotUpdatedResult.asSuccess(), result) },
+            { assertEquals(expectedNotUpdatedResult, nextState) }
+        )
     }
 
     @Test
@@ -160,5 +141,33 @@ class LoadingAndUpdatingStockTests {
         }
     }
 }
+
+private fun List<Item>.withQualityDecreasedBy(qualityChange: Int): List<Item> =
+    map { it.copy(quality = it.quality - qualityChange) }
+
+data class DoLoadAndUpdateOutcome(
+    val result: Result4k<StockList, StockListLoadingError>,
+    val nextState: StockList,
+)
+
+private fun doLoadAndUpdate(
+    initialStockList: StockList,
+    now: Instant,
+): DoLoadAndUpdateOutcome {
+    val items = InMemoryItems(initialStockList)
+    val stock = Stock(items, zoneId = londonZone)
+    val result = items.inTransaction {
+        stock.loadAndUpdateStockList(now)
+    }
+    val nextState = items.inTransaction {
+        items.load().onFailure { error("Should not fail") }
+    }
+    return DoLoadAndUpdateOutcome(result, nextState)
+}
+
+private val someItems = listOf(
+    item("banana", oct29, 42),
+    item("kumquat", oct29, 101)
+)
 
 private val londonZone = ZoneId.of("Europe/London")
